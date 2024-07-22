9d02bc2377480f8e7c135e03775486f6ac4be568
Kz <jaiderevp@ext.inditex.com>
2024-07-22 08:41:53 +0200

    feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-export default function App() {
+import React from 'react';
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button>Button click</button>
+      <a href="https://www.google.com">Google</a>
+    </div>
   );
-}
+};
+
+export default App;
